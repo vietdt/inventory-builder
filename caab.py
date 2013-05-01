0e2975a95832faf24dc85dc92c4d5b238cb17935
vietdt <vietdt@gmail.com>
2013-05-02 02:58:51 +0700

    get title from browser only when subtype is html

--- caab.py
+++ caab.py
@@ -9,23 +9,33 @@ class CaabCrawler(StandardCrawler):
     caab custom crawler
     """
 
-    def process_row(self, url, title='', response=None):
+    def process_row(self, url, response=None):
         """
         Override default behaviour of process_row
         """
         parsed_url = urlparse.urlparse(url)
         path = parsed_url.path
+        # get subtype from response
+        subtype = None
+        if response:
+            subtype = response.info().subtype
         # get url extension
         ext = os.path.splitext(path)[1]
         # calculate type
-        if ext in self.html_exts:
-            itemtype = 'HTML'
+        if subtype:
+            # set itemtype by subtype
+            itemtype = str.upper(subtype)
+        # get itemtype from ext
         elif ext == '.pdf':
             itemtype = 'PDF'
         else:
             itemtype = 'Unknown'
-        # use filename when no title found
-        if not title:
+        # calculate title
+        if subtype == 'html':
+            # get browser title
+            title = self.browser.title()
+        else:
+            # use filename when no title found
             title = urlparse.unquote(path.split('/')[-1])
         # remove filename in path
         path = '/'.join(path.split('/')[:-1])+'/'
@@ -37,6 +47,6 @@ caab_crawler = CaabCrawler('http://caab.org/',
                            media_exts=['.pdf'],
                            output_filename='caab_inv',
                            log_filename='caab_crawler',
-                           timeout=300.0)
+                           timeout=90.0)
 # start crawling
 caab_crawler.run()
