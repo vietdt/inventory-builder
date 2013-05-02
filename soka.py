d6ce53a6209912d948c91c9c73ed0bfcf1fdcdf8
vietdt <vietdt@gmail.com>
2013-05-02 21:31:30 +0700

    skip more urls and extract filename from header

--- soka.py
+++ soka.py
@@ -1,6 +1,9 @@
 import os
+import cgi
 import urlparse
 import logging
+
+from mechanize import _sgmllib_copy as sgmllib
 
 from crawler import StandardCrawler
 
@@ -15,6 +18,10 @@ class SokaCrawler(StandardCrawler):
         """
         # skip language changing link
         if 'set_language=' in url.query: return
+        # skip sendto_form link
+        if url.path.endswith('/sendto_form'): return
+        # skip bookmark link
+        if url.geturl().startswith('#'): return
         # run default validation
         return self._validate_url(url)
 
@@ -24,29 +31,39 @@ class SokaCrawler(StandardCrawler):
         """
         parsed_url = urlparse.urlparse(url)
         path = parsed_url.path
+        title = None
         # get subtype from response
-        subtype = None
+        resp_info = subtype = None
         if response:
-            subtype = response.info().subtype
+            resp_info = response.info()
+            subtype = resp_info.subtype
         # get url extension
         ext = os.path.splitext(path)[1]
         # calculate type
         if subtype:
             # set itemtype by subtype
             itemtype = str.upper(subtype)
+            # calculate title
+            if subtype == 'html':
+                # get browser title
+                try:
+                    title = self.browser.title()
+                except sgmllib.SGMLParseError, e:
+                    # unknown error may occur, skip it
+                    self.logger.warning('title parsing error: %s' % url)
+            else:
+                # get filename from header
+                _, params = cgi.parse_header(resp_info.getheader('Content-Disposition', ''))
+                title = params.get('filename')
         # get itemtype from ext
         elif ext:
             itemtype = ext[1:].upper()
         else:
             itemtype = 'Unknown'
-        # calculate title
-        if subtype == 'html':
-            # get browser title
-            title = self.browser.title()
-        else:
-            # use filename when no title found
+        # use last component in path as title when nothing found
+        if not title:
             title = urlparse.unquote(path.split('/')[-1])
-        # remove filename in path
+        # remove last component in path
         path = '/'.join(path.split('/')[:-1])+'/'
         return itemtype, title, path, url
 
